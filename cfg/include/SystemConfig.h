a0b81c86e3eb67d01e79d37e4230ec34c275bd40
FRGT <forgotname110@gmail.com>
2025-10-01 12:10:30 +0500

    Still making SystemConfig

---
+++ cfg/include/SystemConfig.h
@@ -0,0 +1,25 @@
+#ifndef SYSTEM_CONFIG_H
+#define SYSTEM_CONFIG_H
+
+#include <Eigen/Dense>
+
+namespace cfg {
+struct SystemConfig{
+    static constexpr int numRobots = 12;
+
+    // Ball
+    static const Eigen::Vector3d initBallPosition;
+    static const float ballRadius;
+    
+    // Robots
+    static const float robotRadius;
+
+    // Robot Team
+    static std::vector<Eigen::Vector3d> teamOneStartFormation;
+    static std::vector<Eigen::Vector3d> teamTwoStartFormation;
+
+    
+};
+}
+
+#endif
