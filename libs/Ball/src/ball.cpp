42ea08bcf6d6d5436d3f26934d31c1384bd71133
Muwahid Ayaan <sfmchoudry.ayaan@gmail.com>
2025-10-02 14:16:02 +0500

    Debugging

--- libs/Ball/src/ball.cpp
+++ libs/Ball/src/ball.cpp
@@ -3,11 +3,13 @@
 namespace Ball {
 void Ball::drawBall(QPainter* painter) {
   painter->setBrush(QBrush(Qt::red));
-  painter->setPen(QPen(Qt::NoPen));
-  // if (ballPos.y() < 0 or ballPos.y() + ballDia > cfg::Dimensions::screenHeight) ballV.y() *= -1;
-  // if (ballPos.x() + ballDia > cfg::Dimensions::screenWidth or ballPos.x() < 0) ballV.x() *= -1;
+  painter->setPen(QPen(Qt::black, 0.01));
+  if (ballPos.y() < 0 or ballPos.y() + ballDia > cfg::Dimensions::screenHeight) ballV.y() *= -1;
+  if (ballPos.x() + ballDia > cfg::Dimensions::screenWidth or ballPos.x() < 0) ballV.x() *= -1;
   if (respawnBall) ballPos = initPos, ballV = Eigen::Vector2d(0, 0);
-
-  painter->drawEllipse(ballPos.x(), ballPos.y(), 100, 100);
+  qreal X = ballPos.x();
+  qreal Y = ballPos.y();
+  QPointF Cords(X, Y);
+  painter->drawEllipse(Cords, ballDia, ballDia);
 }
 }  // namespace Ball
