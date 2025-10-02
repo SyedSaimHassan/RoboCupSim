fe3be888805c3b09e9918190ec70becf91f49863
FRGT <forgotname110@gmail.com>
2025-10-02 11:35:25 +0500

    Commented out extra parameters

--- libs/cfg/include/systemConfig.h
+++ libs/cfg/include/systemConfig.h
@@ -14,8 +14,8 @@ struct SystemConfig{
     // Robots
     static const float robotRadius;
     static const float playerMaxSpeed;
-    static const float playerMaxAcceleration;
-    static const float playerMaxShootForce;
+    // static const float playerMaxAcceleration;
+    // static const float playerMaxShootForce;
 
     // Robot Team
     static std::vector<Eigen::Vector3d> teamOneStartFormation;
