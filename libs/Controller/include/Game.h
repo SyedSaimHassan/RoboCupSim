1ef67cf6f1592f2994831d2f47dfd89d8bb84e8b
Muwahid Ayaan <sfmchoudry.ayaan@gmail.com>
2025-10-02 07:52:44 +0500

    hard reset

---
+++ libs/Controller/include/Game.h
@@ -0,0 +1,32 @@
+#ifndef GAME_H
+#define GAME_H
+#include <QApplication>
+#include <QWidget>
+#include <QPainterPath>
+#include <QPainter>
+#include <QScreen>
+#include <QTimer>
+#include <QKeyEvent>
+#include <iostream>
+#include <vector>
+
+#define To_Be_Implemented int
+
+namespace Controller {
+enum class State { Kickoff, Playing, Paused, Foul, Finished };
+class Game : public QWidget {
+ private:
+  State gameState = State::Kickoff;
+
+  void drawField(QPainter *painter);
+  void drawPlayers(QPainter *painter);
+  void drawBall(QPainter *painter);
+  void paintEvent(QPaintEvent *) override;
+
+ public:
+  Game(QWidget *parent = nullptr);
+};
+
+}  // namespace Controller
+
+#endif
---
+++ libs/Controller/src/Game.cpp
@@ -0,0 +1,12 @@
+#include "Game.h"
+
+namespace Controller {
+
+Game::Game(QWidget *parent = nullptr) : QWidget(parent) {
+    
+    std::cout << "[Controller::Game::Game] Startup Successfull" << std::endl;
+    
+}
+
+
+}  // namespace Controller
