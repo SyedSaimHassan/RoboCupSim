078cb54f2f3df6c041312af364888937d66ced0d
UsmanRiaz257 <usmanssasss@gmail.com>
2025-10-14 11:47:06 +0500

    Trajectory

--- libs/Trajectory/src/TrapoizdalTragectory.cpp
+++ libs/Trajectory/src/TrapoizdalTragectory.cpp
@@ -3,10 +3,10 @@ Eigen::Vector3d Tra_Tra::traj(int index){
     Eigen::Vector3d currentpos = cfg::SystemConfig::teamOnePlayerPos[index];
     Eigen::Vector3d currentvel = cfg::SystemConfig::teamOnePlayerVel[index];
     double maxvelincom = pow(pow(cfg::SystemConfig::playerMaxSpeed,2)/2,0.5);
-    if ((waypoint[index-1].size()) == 1){
+    if ((cfg::SystemConfig::teamOneWayPoints[index-1].size()) == 1){
         return Eigen::Vector3d(0,0,0);
     }
-    Eigen::Vector3d goalpos=waypoint[index-1][0];
+    Eigen::Vector3d goalpos=cfg::SystemConfig::teamOneWayPoints[index-1][0];
     double remaing_dis = pow((pow(currentpos.x()-goalpos.x(),2))+(pow(currentpos.y()-goalpos.y(),2)),0.5);
     double dis_travl = (pow(currentvel.x(),2)+pow(currentvel.y(),2))/2*cfg::SystemConfig::playerMaxAcceleration;
     double finalvelX;
