71304d0268dfb9729022f6fc66a2b3e9dd7579fe
UsmanRiaz257 <usmanssasss@gmail.com>
2025-10-14 11:15:38 +0500

    ,

--- libs/Trajectory/include/TrapoizdalTragectory.h
+++ libs/Trajectory/include/TrapoizdalTragectory.h
@@ -2,6 +2,6 @@
 #include<cmath>
 class Tra_Tra{
     public:
-    double dt=1
+    double dt=1/cfg::SystemConfig::frameRate;
     Eigen::Vector3d traj(int index);
-}
+};
--- libs/Trajectory/src/TrapoizdalTragectory.cpp
+++ libs/Trajectory/src/TrapoizdalTragectory.cpp
@@ -2,13 +2,23 @@
 Eigen::Vector3d Tra_Tra::traj(int index){
     Eigen::Vector3d currentpos = cfg::SystemConfig::teamOnePlayerPos[index];
     Eigen::Vector3d currentvel = cfg::SystemConfig::teamOnePlayerVel[index];
-    double maxvel = cfg::SystemConfig::playerMaxSpeed;
-    Eigen::Vector3d goalpos=Waypoint[0][0];
-    double remaing_dis = pow((pow(currentpos.x()-goalpos.x(),2))+((pow(currentpos.y()-goalpos.y(),2)),0.5));
+    double maxvelincom = pow(pow(cfg::SystemConfig::playerMaxSpeed,2)/2,0.5);
+    if ((waypoint[index-1].size()) == 1){
+        return Eigen::Vector3d(0,0,0);
+    }
+    Eigen::Vector3d goalpos=waypoint[index-1][0];
+    double remaing_dis = pow((pow(currentpos.x()-goalpos.x(),2))+(pow(currentpos.y()-goalpos.y(),2)),0.5);
     double dis_travl = (pow(currentvel.x(),2)+pow(currentvel.y(),2))/2*cfg::SystemConfig::playerMaxAcceleration;
-    
+    double finalvelX;
+    double finalvelY;
+    double maxaccincom=pow(pow(cfg::SystemConfig::playerMaxAcceleration,2)/2,0.5);
     if (remaing_dis>dis_travl){
-        
+        finalvelX=std::min(currentvel.x()+maxaccincom*dt,maxvelincom);
+        finalvelX=std::min(currentvel.x()+maxaccincom*dt,maxvelincom);
     }   
-
+    else{
+        finalvelX=std::max(currentvel.x()-maxaccincom*dt,0.0);
+        finalvelX=std::max(currentvel.x()-maxaccincom*dt,0.0);
+    }
+    return Eigen::Vector3d(finalvelX,finalvelY,currentvel.z());
 }
