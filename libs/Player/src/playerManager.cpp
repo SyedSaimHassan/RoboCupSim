626ddc7da67f271646522edcaf64603c75da50e8
Muwahid Ayaan <sfmchoudry.ayaan@gmail.com>
2025-10-14 12:00:19 +0500

    Rebasing

--- libs/Player/src/playerManager.cpp
+++ libs/Player/src/playerManager.cpp
@@ -5,39 +5,14 @@ void PlayerManager::ManagePlayers(QPainter *p, QSet<int> PlayerKeys) {
   for (int RobotIndex = 1; RobotIndex < int(cfg::SystemConfig::numRobots / 2); RobotIndex++) {
     movePlayer(RobotIndex);
   }
-  Render(p);
-}
 
-void PlayerManager::detectCollision() {
-  // for (int RobotIndex1 = 1; RobotIndex1 <= int(cfg::SystemConfig::numRobots / 2); RobotIndex1++)
-  // {
-  //   for (int RobotIndex2 = RobotIndex1 + 1; RobotIndex2 <= int(cfg::SystemConfig::numRobots /
-  //   2);
-  //        RobotIndex2++) {
-  //     Eigen::Vector3d pos1 = Players::getPose(RobotIndex1);
-  //     Eigen::Vector3d pos2 = Players::getPose(RobotIndex2);
-  //     if (util::withinRadius(playerPositions[RobotIndex1 - 1], playerPositions[RobotIndex2 -
-  //     1])) {
-  //       deflectPlayers(RobotIndex1, RobotIndex2);
-  //       std::cout << RobotIndex1 << ' ' << RobotIndex2 << ' '
-  //                 << util::euclideanDistance(QPointF(pos1.x(), pos1.y()),
-  //                                            QPointF(pos2.x(), pos2.y()))
-  //                 << std::endl;
-  //     }
-  //   }
-  // }
-  // Eigen::Vector3d pos1 = Players::getPose(2);
-  // Eigen::Vector3d pos2 = Players::getPose(1);
-  // std::cout << pos1.x() << ' ' << pos1.y() << ' ' << pos2.x() << ' ' << pos2.y() << std::endl;
-  // std::cout << util::euclideanDistance(QPointF(pos1.x(), pos1.y()), QPointF(pos2.x(), pos2.y()))
-  //           << std::endl;
+  Render(p);
 }
 
 QPointF changeHelper(Eigen::Vector3d vector) { return QPointF(vector.x(), vector.y()); }
 
 void PlayerManager::deflectPlayers(int PlayerID1, int PlayerID2) {
-
-  //avoiding overlap
+  // avoiding overlap
   auto pos1 = getPose(PlayerID1);
   auto pos2 = getPose(PlayerID2);
 
@@ -54,11 +29,9 @@ void PlayerManager::deflectPlayers(int PlayerID1, int PlayerID2) {
 
   setPose(pos1, PlayerID1);
   setPose(pos2, PlayerID2);
-  //avoiding overlap
+  // avoiding overlap
 
-  //deflection
-  
-
+  // deflection
 }
 void PlayerManager::handleAuto(int PlayerInd) {}
 void PlayerManager::handleManual(int PlayerInd) {
@@ -105,20 +78,6 @@ void PlayerManager::handleManual(int PlayerInd) {
   SetPlayerV(PlayerVel, PlayerInd);
 }
 void PlayerManager::movePlayer(int playerID = 0) {
-  // detecting collision
-  for (int RobotIndex1 = 1; RobotIndex1 <= int(cfg::SystemConfig::numRobots / 2); RobotIndex1++) {
-    for (int RobotIndex2 = RobotIndex1 + 1; RobotIndex2 <= int(cfg::SystemConfig::numRobots / 2);
-         RobotIndex2++) {
-      Eigen::Vector3d pos1 = Players::getPose(RobotIndex1);
-      Eigen::Vector3d pos2 = Players::getPose(RobotIndex2);
-      if (util::euclideanDistance(playerPositions[RobotIndex1 - 1],
-                                  playerPositions[RobotIndex2 - 1]) <= 0.408) {
-        deflectPlayers(RobotIndex1, RobotIndex2);
-      }
-    }
-  }
-  // -------------x-------------x----------//
-
   switch (cfg::SystemConfig::PlayerStates[playerID - 1]) {
     case cfg::SystemConfig::RobotState::Autonomous:
       handleAuto(playerID);
@@ -137,7 +96,6 @@ void PlayerManager::movePlayer(int playerID = 0) {
   playerGyroAngle[playerID - 1] =
       playerGyroAngle[playerID - 1] + playerVelocities[playerID - 1].z();
 
-
   // Decel for x
   float playerXDec = std::min(std::abs(float(playerVelocities[playerID - 1].x())),
                               cfg::SystemConfig::playerMaxAcceleration);
@@ -147,8 +105,6 @@ void PlayerManager::movePlayer(int playerID = 0) {
   playerVelocities[playerID - 1].x() += playerXDec;
   // End decel
 
-
-
   float playerYDec = std::min(std::abs(float(playerVelocities[playerID - 1].y())),
                               cfg::SystemConfig::playerMaxAcceleration);
   if (playerVelocities[playerID - 1].y() > 0) {
@@ -156,17 +112,27 @@ void PlayerManager::movePlayer(int playerID = 0) {
   }
   playerVelocities[playerID - 1].y() += playerYDec;
 
-  
-  
   float playerZDec = std::min(std::abs(float(playerVelocities[playerID - 1].z())),
                               cfg::SystemConfig::playerMaxOmegaAcceleration);
   if (playerVelocities[playerID - 1].z() > 0) {
     playerZDec *= -1;
   }
   playerVelocities[playerID - 1].z() += playerZDec;
-
+  
+  // detecting collision
+  for (int RobotIndex1 = 1; RobotIndex1 <= int(cfg::SystemConfig::numRobots / 2); RobotIndex1++) {
+    for (int RobotIndex2 = RobotIndex1 + 1; RobotIndex2 <= int(cfg::SystemConfig::numRobots / 2);
+         RobotIndex2++) {
+      Eigen::Vector3d pos1 = Players::getPose(RobotIndex1);
+      Eigen::Vector3d pos2 = Players::getPose(RobotIndex2);
+      if (util::euclideanDistance(playerPositions[RobotIndex1 - 1],
+                                  playerPositions[RobotIndex2 - 1]) <= 0.408) {
+        deflectPlayers(RobotIndex1, RobotIndex2);
+      }
+    }
+  }
+  // -------------x-------------x----------//
 }
-
 
 Eigen::Vector3d PlayerManager::getPlayerV(int PlayerID) { return playerVelocities[PlayerID - 1]; }
 void PlayerManager::SetPlayerV(Eigen::Vector3d PlayerV, int PlayerID) {
