a7e5dadd2f57fa3a57468c5fba9848576a10fcc0
UsmanRiaz257 <usmanssasss@gmail.com>
2025-10-21 11:20:12 +0500

    worked on kin

--- libs/Kin/include/inv.h
+++ libs/Kin/include/inv.h
@@ -1,6 +1,9 @@
 #include"systemConfig.h"
 #include<vector>
+#include <cmath>
+
 class Inv{
     public:
         std::vector<Eigen::Vector4d> getVelocity();
+        Eigen::Vector4d velocityToRpm(Eigen::Vector4d velcoities);
 };
--- libs/Kin/src/Inv.cpp
+++ libs/Kin/src/Inv.cpp
@@ -3,12 +3,34 @@
 #include "inv.h"
 
 std::vector<Eigen::Vector4d> Inv:: getVelocity(){
+
     std::vector<Eigen::Vector4d> velocities;
-    std::vector<Eigen::Vector3d>playerVol=cfg::SystemConfig::teamOnePlayerVel;
-    for (int i=0;i<playerVol.size();i++){
-        double angle=cfg::SystemConfig::teamOnePlayerPos[i].z();
+    std::vector<double>thetas;
+    double radius=cfg::SystemConfig::distanceFromRobotCenter;
+    
+    for (int i=0;i<cfg::SystemConfig::numRobots;i++){
         
+        Eigen::Vector3d playerVol=cfg::SystemConfig::teamOnePlayerVel[i];
+        double angle=cfg::SystemConfig::teamOnePlayerPos[i].z();
+        Eigen::Matrix<double, 4, 3> A;
+
+        A << -sin(thetas[0]+angle),cos(angle+thetas[0]),radius,
+              -sin(thetas[1]+angle),cos(angle+thetas[1]),radius,
+              -sin(thetas[2]+angle),cos(angle+thetas[2]),radius,
+              -sin(thetas[3]+angle),cos(angle+thetas[3]),radius;
+          
+        
+        velocities.push_back(velocityToRpm(A*playerVol));
     }
+    return velocities;
+}
+
+Eigen::Vector4d Inv::velocityToRpm(Eigen::Vector4d velocities){
+    Eigen::Vector4cd RpmVel;
+    for (int i=0;i<velocities.size();i++){
+        RpmVel(i)=(velocities(i)*60)/(M_PI*cfg::SystemConfig::wheelDiameter);
+    }
+    return RpmVel;
 }
 
 #endif
