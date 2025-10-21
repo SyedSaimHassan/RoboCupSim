c173dd90e0b4f7273227752845bc81d5e761fc33
UsmanRiaz257 <usmanssasss@gmail.com>
2025-10-21 11:24:07 +0500

    worked on Kin

--- libs/Kin/src/Inv.cpp
+++ libs/Kin/src/Inv.cpp
@@ -12,15 +12,15 @@ std::vector<Eigen::Vector4d> Inv:: getVelocity(){
         
         Eigen::Vector3d playerVol=cfg::SystemConfig::teamOnePlayerVel[i];
         double angle=cfg::SystemConfig::teamOnePlayerPos[i].z();
-        Eigen::Matrix<double, 4, 3> A;
+        Eigen::Matrix<double, 4, 3> T;
 
-        A << -sin(thetas[0]+angle),cos(angle+thetas[0]),radius,
+        T << -sin(thetas[0]+angle),cos(angle+thetas[0]),radius,
               -sin(thetas[1]+angle),cos(angle+thetas[1]),radius,
               -sin(thetas[2]+angle),cos(angle+thetas[2]),radius,
               -sin(thetas[3]+angle),cos(angle+thetas[3]),radius;
           
         
-        velocities.push_back(velocityToRpm(A*playerVol));
+        velocities.push_back(velocityToRpm(T*playerVol));
     }
     return velocities;
 }
@@ -28,7 +28,7 @@ std::vector<Eigen::Vector4d> Inv:: getVelocity(){
 Eigen::Vector4d Inv::velocityToRpm(Eigen::Vector4d velocities){
     Eigen::Vector4cd RpmVel;
     for (int i=0;i<velocities.size();i++){
-        RpmVel(i)=(velocities(i)*60)/(M_PI*cfg::SystemConfig::wheelDiameter);
+        RpmVel(i)=(velocities(i)*60)/(M_PI*cfg::SystemConfig::w);
     }
     return RpmVel;
 }
